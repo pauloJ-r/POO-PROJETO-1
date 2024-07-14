3b6664d32e00e6192301016d13f1e6c96b23a394
pauloJ-r <pj74586@gmail.com>
2024-07-14 18:09:22 -0300

    feat: adicionando metodos de remConta, localizarConta, getBalanco

--- src/banco/logica/Cliente.java
+++ src/banco/logica/Cliente.java
@@ -1,7 +1,9 @@
 package banco.logica;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Cliente implements Serializable {
 
@@ -25,7 +27,55 @@ public class Cliente implements Serializable {
 			this.contas.add(c);
 
 	}
+	public void remConta(Conta c) {
+		if(contas.contains(c))
+			this.contas.remove(c);
+		else
+			System.out.println("Conta inexistente");
+	}
+	public Conta localizarConta(String numero) {
+		for(Conta c : contas) {
+		   if(c.getNumero().equals(numero)) {
+			  return c;
+		   }
+		}
+		return null;
+	}
 	
+	public ArrayList<Conta> getContas() {
+		return contas;
+	}
+
+    public BigDecimal getBalanco() {
+    	BigDecimal balanco = BigDecimal.ZERO;
+    	for (Conta c : contas) {
+    		balanco = balanco.add(c.getSaldo());
+    	}
+    	return balanco;
+    }
+
+	@Override
+	public String toString() {
+		return "Cliente [cpf=" + cpf + ", nome=" + nome + ", contas=" + contas + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
 
 	public String getCpf() {
 		return cpf;
